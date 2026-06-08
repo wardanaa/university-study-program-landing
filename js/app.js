0d0e84ce3f57e629b1896af91dbfa2dd24fc1528
Made Wardana <made.wardana44@gmail.com>
2026-06-08 21:34:06 +0800

    feat: register alpine.js components in app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 document.addEventListener("alpine:init", () => {
-  window.siteApp = {
-    initialized: true,
-  };
-});
+  const getLandingPageData = () => window.landingPageData ?? window.siteData ?? {};
 
+  const createComponent = (state = {}) => () => ({
+    landingPageData: getLandingPageData(),
+    ...state,
+  });
+
+  Alpine.data("announcementBar", createComponent());
+  Alpine.data("mobileNav", createComponent({ isOpen: false }));
+  Alpine.data("stickyCta", createComponent());
+  Alpine.data("videoModal", createComponent({ isOpen: false, activeVideo: null }));
+  Alpine.data("bentoGrid", createComponent());
+  Alpine.data("curriculumRoadmap", createComponent());
+  Alpine.data("careerExplorer", createComponent());
+  Alpine.data("projectFilter", createComponent({ activeFilter: "all" }));
+  Alpine.data("alumniModal", createComponent({ isOpen: false, activeAlumni: null }));
+  Alpine.data("faqAssistant", createComponent({ activeIndex: null }));
+  Alpine.data("scrollReveal", createComponent({ isVisible: false }));
+});
